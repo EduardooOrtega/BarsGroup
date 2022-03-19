51e396006676c705e354a6ad3cbbfa27833fa83d
i.tsyuryupa <openingthedoor@yandex.ru>
2022-03-19 22:01:53 +0300

    Add HomeWork Event Add Class Read and fill

---
+++ Event/Read.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    internal class Read
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            char input = ' ';
+            while (input != 'c' || input != 'с')
+            {
+                input = Console.ReadKey().KeyChar;
+                OnKeyPressed?.Invoke(this, input);
+            }
+        }
+    }
+}
