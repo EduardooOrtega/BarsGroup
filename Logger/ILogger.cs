979c6093ef52e712ee4e249fb8a44af2a1e2ea3f
i.tsyuryupa <openingthedoor@yandex.ru>
2022-03-27 09:59:13 +0300

    Add Project Logger Create interface ILogger Created a LocalFileLogger class that implements ILogger

---
+++ Logger/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    internal interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+
+        void LogError(string message, Exception ex);
+    }
+
+}
---
+++ Logger/LocalFileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    internal class LocalFileLogger<T> : ILogger
+    {
+        private string Path;
+
+        public string GenericTypeName = typeof(T).Name;
+
+        public LocalFileLogger(string _path)
+        {
+            Path = _path;
+        }
+
+        public void LogInfo(string message)
+        {
+            File.WriteAllText(Path, $"[Info]: [{GenericTypeName}] : {message}");
+        }
+
+        public void LogWarning(string message)
+        {
+            File.WriteAllText(Path, $"[Warning] : [{GenericTypeName}] : {message}");
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            File.WriteAllText(Path, $"[Error] : [{GenericTypeName}] : {message}. {ex.Message}");
+        }
+    }
+
+}
