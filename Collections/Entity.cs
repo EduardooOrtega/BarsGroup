020ab9fe1ed7bd187f33d51961c78501532d650b
i.tsyuryupa <openingthedoor@yandex.ru>
2022-03-27 15:09:45 +0300

    Add Project Collections Create an Entity class that implements the IComparable interface to run queries
    Check work

---
+++ Collections/Entity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Entity: IComparable<Entity>
+    {
+        public int Id;
+
+        public int ParentId;
+
+        public string Name;
+
+        public int CompareTo(Entity e)
+        {
+            if (this.Id < e.Id)
+            {
+                return -1;
+            }
+            else if(this.Id > e.Id)
+            {
+                return 1;
+            }
+
+            return 0;
+
+        }
+
+        static public Dictionary<int, List<Entity>> Sort(List<Entity> ListEntity)
+        {
+            Dictionary<int, List<Entity>> dictionary = new Dictionary<int, List<Entity>>();
+
+            var query = ListEntity.OrderBy(Id => Id).Select(item => item);
+
+            int previousParentId = 0;
+            foreach (var item in query)
+            {
+                //if (item.ParentId != previousParentId)
+                //{
+                //    dictionary.Add(item.ParentId, new List<Entity>);
+                //}
+                Console.WriteLine($"{ item.ParentId}{ item.Id}");
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿using Collections;
+
+Entity e1 = new Entity { Id = 1, ParentId = 0, Name = "Root entity" };
+
+
+Entity e2 = new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"};
+
+
+Entity e3 = new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"};
+
+
+Entity e4 = new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"};
+
+
+Entity e5 = new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"};
+
+List<Entity> entities = new List<Entity> { e1, e2, e3, e4, e5 };
+
+Entity.Sort(entities);
